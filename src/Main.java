a833ea26eaa71d6cc66be4cc608f735c9a7d4c0b
Marina <rms.marina89@gmail.com>
2021-09-08 15:17:49 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int currentBalance = 2000000000;
+        int transferAmount = 500000000;
+        int totalAmount = currentBalance + transferAmount;
+        System.out.println(totalAmount);
+    }
+}
